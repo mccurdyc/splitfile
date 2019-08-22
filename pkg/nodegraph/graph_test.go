63cc93118b7b6e8902cb8a60aaf6845376d1fdd9
Colton McCurdy <mccurdyc22@gmail.com>
2019-08-22 08:09:37 -0400

    add AddNodes and AddEdges tests
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- pkg/nodegraph/graph_test.go
+++ pkg/nodegraph/graph_test.go
@@ -31,7 +31,7 @@ type mockNode struct {
 	pos int
 }
 
-func (m *mockNode) Pos() token.Pos {
+func (m mockNode) Pos() token.Pos {
 	return token.Pos(m.pos)
 }
 
@@ -42,11 +42,25 @@ func TestAddNodes(t *testing.T) {
 		nodes []Node
 	}{
 		{
+			name: "add nil node",
+			graph: &Graph{
+				relations: make(map[Node][]Node),
+			},
+			nodes: []Node{nil},
+		},
+		{
 			name: "add single node",
 			graph: &Graph{
 				relations: make(map[Node][]Node),
 			},
-			nodes: []Node{&mockNode{pos: 1}},
+			nodes: []Node{mockNode{pos: 1}},
+		},
+		{
+			name: "add multiple nodes",
+			graph: &Graph{
+				relations: make(map[Node][]Node),
+			},
+			nodes: []Node{mockNode{pos: 1}, mockNode{pos: 2}, mockNode{pos: 3}},
 		},
 	}
 
@@ -55,12 +69,105 @@ func TestAddNodes(t *testing.T) {
 			expected := make(map[Node][]Node)
 
 			for _, n := range test.nodes {
-				expected[n] = make([]Node, 0, 1)
+				expected[n] = make([]Node, 0, 0)
 			}
 
 			test.graph.AddNodes(test.nodes...)
 
 			assert.Equal(t, expected, test.graph.relations, "the expected map does not match the graphs relation map.")
+		})
+	}
+}
+
+func TestAddEdges(t *testing.T) {
+	tests := []struct {
+		name     string
+		graph    *Graph
+		source   Node
+		related  []Node
+		expected *Graph
+	}{
+		{
+			name: "nil source",
+			graph: &Graph{
+				relations: make(map[Node][]Node),
+			},
+			source:  nil,
+			related: []Node{mockNode{pos: 1}},
+			expected: &Graph{
+				relations: make(map[Node][]Node),
+			},
+		},
+		{
+			name: "nil related non-existent source",
+			graph: &Graph{
+				relations: make(map[Node][]Node),
+			},
+			source:  mockNode{pos: 1},
+			related: nil,
+			expected: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+		},
+		{
+			name: "nil related source exists",
+			graph: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+			source:  mockNode{pos: 1},
+			related: nil,
+			expected: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+		},
+		{
+			name: "adding single edge to existing node",
+			graph: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+			source:  mockNode{pos: 1},
+			related: []Node{mockNode{pos: 2}},
+			expected: &Graph{
+				relations: map[Node][]Node{
+					mockNode{pos: 1}: []Node{mockNode{pos: 2}},
+					mockNode{pos: 2}: make([]Node, 0),
+				},
+			},
+		},
+		{
+			name: "adding single edge to non-existent node",
+			graph: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+			source:  mockNode{pos: 2},
+			related: []Node{mockNode{pos: 3}},
+			expected: &Graph{
+				relations: map[Node][]Node{
+					mockNode{pos: 1}: make([]Node, 0),
+					mockNode{pos: 2}: []Node{mockNode{pos: 3}},
+					mockNode{pos: 3}: make([]Node, 0),
+				},
+			},
+		},
+		{
+			name: "source and related are the same node",
+			graph: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+			source:  mockNode{pos: 1},
+			related: []Node{mockNode{pos: 1}},
+			expected: &Graph{
+				relations: map[Node][]Node{mockNode{pos: 1}: make([]Node, 0)},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			test.graph.AddEdges(test.source, test.related...)
+
+			assert.Equal(t, test.expected, test.graph, "the expected graph does not match the actual graph after adding edges.")
 		})
 	}
 }
