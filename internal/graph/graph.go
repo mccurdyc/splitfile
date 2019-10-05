0142c039f8d36a08aa557916a4081d6f7a739db1
Colton McCurdy <mccurdyc22@gmail.com>
2019-10-05 16:12:57 -0400

    internal/graph: adding graph package
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

---
+++ internal/graph/graph.go
@@ -0,0 +1,28 @@
+package graph
+
+import "github.com/pkg/errors"
+
+// Graph is a map of node IDs to the Node with that ID.
+type Graph map[string]*Node
+
+// New creates a pointer to a Graph and initializes a map of nodes.
+func New() Graph {
+	g := make(map[string]*Node)
+	return Graph(g)
+}
+
+// AddNode adds a valid node to the Graph.
+func (g Graph) AddNode(n *Node) error {
+	if ok, err := n.Valid(); !ok {
+		return errors.Wrap(err, "could not add invalid node")
+	}
+
+	g[n.ID] = n
+	return nil
+}
+
+// ContainsNode returns whether or not the graph contains a node with the given id.
+func (g Graph) ContainsNode(id string) bool {
+	_, ok := g[id]
+	return ok
+}
---
+++ internal/graph/node.go
@@ -0,0 +1,60 @@
+package graph
+
+import "errors"
+
+// WeightedEdge contains pointers to source and destination Nodes as well as
+// the assigned weight of the relationship.
+type WeightedEdge struct {
+	Weight float64
+	Dest   *Node
+}
+
+// Node has an ID and a map of WeightedEdges or weighted relationships to other
+// Nodes.
+type Node struct {
+	ID    string
+	Edges map[string]WeightedEdge
+}
+
+// NewNode creates a pointer to a new Node with ID, id, and initializes a map of Edges.
+func NewNode(id string) *Node {
+	return &Node{
+		ID:    id,
+		Edges: make(map[string]WeightedEdge),
+	}
+}
+
+// AddEdges adds weighted edges to a source node that signify relationships with other nodes.
+func (n *Node) AddEdge(dest *Node, w float64) {
+	// prevent edge between node and itself
+	if n.ID == dest.ID {
+		return
+	}
+
+	n.Edges[dest.ID] = WeightedEdge{
+		Weight: w,
+		Dest:   dest,
+	}
+}
+
+// ContainsEdge returns whether or not the graph contains an edge from source to dest.
+func (n *Node) ContainsEdge(dest *Node) bool {
+	_, ok := n.Edges[dest.ID]
+	return ok
+}
+
+func (n *Node) Valid() (bool, error) {
+	if n == nil {
+		return false, errors.New("invalid node; node cannot be nil")
+	}
+
+	if len(n.ID) == 0 {
+		return false, errors.New("invalid node; node must have an ID")
+	}
+
+	if n.Edges == nil {
+		return false, errors.New("invalid node; edge map must be initialized")
+	}
+
+	return true, nil
+}
