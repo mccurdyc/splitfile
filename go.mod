e96731770eefaf8c5563cb5d1a7fd4431e0750d9
Colton McCurdy <mccurdyc22@gmail.com>
2019-10-15 09:03:34 -0400

    internal/graph: work in progress on partition algorithm
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/google/go-cmp v0.3.1
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.3.0
-	golang.org/x/tools v0.0.0-20190826060629-95c3470cfb70
+	golang.org/x/tools v0.0.0-20190918214516-5a1a30219888
+	golang.org/x/tools/gopls v0.1.7 // indirect
 	honnef.co/go/tools v0.0.1-2019.2.3
 )
--- internal/graph/graph.go
+++ internal/graph/graph.go
@@ -1,8 +1,28 @@
 package graph
 
 import (
+	"container/list"
+
 	"github.com/pkg/errors"
 )
+
+// Weights defines the configurable edge weights
+type Weights struct {
+	// TypeField defines the edge weight of a type that has a field of another type.
+	TypeField float64
+	// TypeMethod defines the edge weight of a type that is a func/method receiver.
+	TypeMethod float64
+	// VarOfType defines the edge weight between a variable and the variable's type.
+	VarOfType float64
+	// ConstOfType defines the edge weight between a constant and the constant's type.
+	ConstOfType float64
+	// FuncParam defines the edge weight between a function parameter and the parameter's type.
+	FuncParam float64
+	// FuncReturn defines the edge weight between a function return value and the value's type.
+	FuncReturn float64
+	// FuncBody defines the edge weight between a value used in a function body and the value's type.
+	FuncBody float64
+}
 
 // Graph is a map of node IDs to the Node with that ID.
 type Graph map[string]*Node
@@ -30,17 +50,48 @@ func (g Graph) ContainsNode(id string) bool {
 }
 
 // Partition returns a slice of nodes that should be split from a given source graph.
-//
-// TODO (Issue#8): actually add logic to properly partition
-func (g Graph) Partition() []*Node {
-	res := make([]*Node, 0, len(g))
+// thoughts:
+// 1. we dont necessarily know how the graph is structured; we do know the nodes
+//   a. this is why we have to traverse from every node as the root
+// 2. we need the distance between every pair of _connected_ nodes
+//   a. we have all of the edge weights
+//   b. we dont yet know about connectedness (we'll find that out here when we traverse the graph)
+// 3. calculate distance of every root -> leaf
+//   a. return paths and their weights
+func (g Graph) Partition(epsilon float64) [][]Node {
+	visited := make(map[string]map[string][]float64)
 
-	// TODO: for now, just return every node
-	for _, v := range g {
-		res = append(res, v)
+	for _, root := range g {
+		calculateDistance := func(a, b *Node, m map[string]map[string][]float64) {
+			m[a.ID][b.ID] = append(m[a.ID][b.ID], .Weight)
+		}
+
+		bfs(root, visited)
+	}
+}
+
+func bfs(root *Node, visited map[string]map[string][]float64, fn func(*Node, *Node, map[string]map[string][]float64)) {
+	queue := list.New()
+	visited[root.ID] = make(map[string][]float64) // paths [root][edge] and their distances
+
+	for _, edge := range root.Edges {
+		if _, ok := visited[edge.Dest.ID]; ok {
+			fn(root, edge.Dest, visited)
+			continue
+		}
+
+		queue.PushBack(edge.Dest)
 	}
 
-	return res
+	for queue.Len() > 0 {
+		e := queue.Front()
+		n, ok := e.Value.(*Node)
+		if !ok {
+			continue
+		}
+
+		bfs(n, visited, fn)
+	}
 }
 
 func (g Graph) shouldPartition() bool {
