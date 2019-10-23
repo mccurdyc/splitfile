07e772296d9ffecc330687658ccc9c0b21ee5654
Colton McCurdy <mccurdyc22@gmail.com>
2019-10-23 07:12:14 -0400

    testdata: using the example from the README:
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- testdata/src/abc/abc.go
+++ testdata/src/abc/abc.go
@@ -1,40 +1,13 @@
 package abc
 
-type Ta struct {
-	Tb Tb
-}
+type (
+	A int
+	B int
+	C int
+)
 
-func (Ta) M1() {}
-func (Ta) M2() {}
-func Fa() Ta {
-	var ta Ta
-	return ta
-}
+func fabc(a A, b B, c C) {}
 
-type Tb int
-
-func (Tb) M1() {}
-func FCommon() {
-	var ta Ta
-	var tb Tb
-
-	if ta.Tb == 0 || tb == 0 {
-		return
-	}
-	return
-}
-
-func FCommonParams(ta Ta, tb Tb) {
-	return
-}
-
-type tNonExported int
-
-func (tNonExported) M1()    {}
-func (tNonExported) M2()    {}
-func (t *tNonExported) M3() {}
-func (t *tNonExported) M4() {}
-func F() *tNonExported {
-	t := tNonExported(1)
-	return &t
-}
+func (a A) ma(b B) {}
+func (b B) mb()    {}
+func (c C) mc(b B) {}
